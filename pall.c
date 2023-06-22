962fe95bbe4936c9c2cc792ff43fc7dde38b874a
Mohamed Madeeh <mohamedmadeeh1106@gmail.com>
2023-06-22 14:44:40 -0700

    d

--- pall.c
+++ pall.c
@@ -1,21 +1,19 @@
 #include "monty.h"
-/**
- * f_pall - prints the stack
- * @head: stack head
- * @counter: no used
- * Return: no return
-*/
-void f_pall(stack_t **head, unsigned int counter)
-{
-	stack_t *h;
-	(void)counter;
 
-	h = *head;
-	if (h == NULL)
-		return;
-	while (h)
+/**
+ * op_pall - prints all values on the stack
+ * @stack: pointer to the top of the stack
+ * @line_number: line number of the opcode
+ */
+void op_pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current = *stack;
+
+	while (current != NULL)
 	{
-		printf("%d\n", h->n);
-		h = h->next;
+		printf("%d\n", current->n);
+		current = current->next;
+		(void)line_number;
 	}
+
 }
